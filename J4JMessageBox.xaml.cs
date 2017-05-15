0b0a43135408bf812da5987c1ebdf04cdf48de13
Mark Olbert <mark@arcabama.com>
2017-05-15 14:38:30 -0700

    save changes prior to branching to test eliminating AutoFac from WixUI

--- J4JMessageBox.xaml.cs
+++ J4JMessageBox.xaml.cs
@@ -1,23 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using GalaSoft.MvvmLight.Command;
-using Olbert.JumpForJoy;
-using Olbert.JumpForJoy.Annotations;
 
-namespace Olbert.LanHistory
+namespace Olbert.JumpForJoy.WPF
 {
     /// <summary>
     /// Interaction logic for J4JMessageBox.xaml
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Olbert.JumpForJoy.Properties {
+namespace Olbert.JumpForJoy.WPF.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Olbert.JumpForJoy.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Olbert.JumpForJoy.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Olbert.JumpForJoy.WPF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
