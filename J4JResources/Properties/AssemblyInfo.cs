a8dde846cb6518cedc9799bd492e63767455e169
Mark Olbert <mark@arcabama.com>
2017-06-03 10:42:17 -0700

    removed j4jresources so I could recreate it

---
+++ J4JResources/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jump for Joy Software Default WPF Resources")]
+[assembly: AssemblyDescription("a collection of resources used in Jump for Joy Software WPF programs and libraries")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jump for Joy Software")]
+[assembly: AssemblyProduct("Jump for Joy Software Default WPF Resources")]
+[assembly: AssemblyCopyright("(c) 2017 Mark A. Olbert all rights reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c361dd9-1bfe-4364-bde1-bd1f3f2e1c72")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
